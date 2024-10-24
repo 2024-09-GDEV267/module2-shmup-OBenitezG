fa8cf46f9c4e5dd82049d354df6198ea9f72f67a
Obdulio Benitez Garcia <112982014+OBenitezG@users.noreply.github.com>
2024-10-24 13:09:39 -0400

    lost cities

---
+++ Assets/Lost Cities/Card.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum Colour
+{
+    Blue,
+    Green,
+    White,
+    Yellow,
+    Red
+} 
+
+public class Card : MonoBehaviour
+{
+    public Colour colour;
+    public int value;
+
+    // Constructor(colour, value)
+    // self.color = color
+    // self.value = value
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Lost Cities/Deck.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    // List Deck = []
+
+
+    // Constructor()
+    // for every colour in colour enum
+    //  for every value in range(inclusive 1 - 10)
+    //      
+    //      if value == 1 {
+    //          for agreement in range 3
+    //              Card card = new Card(colour, value)
+    //              deck.append(card) 
+    //      else {
+    //          Card card = new Card(colour, value)
+    //          deck.append(card)
+
+    // Call shuffle method
+
+
+    public void Shuffle()
+    {
+        // deck = Random.Shuffle(deck)
+    }
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+}
